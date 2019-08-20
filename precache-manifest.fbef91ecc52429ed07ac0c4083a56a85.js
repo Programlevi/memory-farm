5be3daa0bdad78d896c9041498430751b3a1940b
programLevi <programLevi@gmail.com>
2019-08-20 18:11:13 +0100

    Updates

---
+++ precache-manifest.fbef91ecc52429ed07ac0c4083a56a85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15eb09233fce84508e8228271193d285",
+    "url": "/memory-farm/index.html"
+  },
+  {
+    "revision": "d7c2a8c8e100f66ba824",
+    "url": "/memory-farm/static/css/main.ecb45e3d.chunk.css"
+  },
+  {
+    "revision": "bd0784e2b94eb110a17b",
+    "url": "/memory-farm/static/js/2.603a05a1.chunk.js"
+  },
+  {
+    "revision": "d7c2a8c8e100f66ba824",
+    "url": "/memory-farm/static/js/main.03dd03ca.chunk.js"
+  },
+  {
+    "revision": "49afe4e5c4811c974a45",
+    "url": "/memory-farm/static/js/runtime~main.9a917e5e.js"
+  }
+]);
